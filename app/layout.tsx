c21b61a939e32e468d997705a5759c1b5774da9f
dohmeid <doha.hmeid@gmail.com>
2025-09-27 12:19:27 +0300

    enhancing the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import  Navbar  from "./components/Navbar";
-import  Footer  from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Amana Transportation",
-  description: "Bus schedules and tracking for Amana.",
+  description: "Live bus tracking and schedules",
 };
 
 export default function RootLayout({
@@ -17,10 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className} style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
         <Navbar />
-        <main>{children}</main>
+        {children}
         <Footer />
       </body>
     </html>
