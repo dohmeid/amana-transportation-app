5194f956371a5837f97b49446f74bc9ffaa34894
dohmeid <doha.hmeid@gmail.com>
2025-09-27 21:46:12 +0300

    changing the website icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Amana Transportation",
   description: "Live bus tracking and schedules",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
